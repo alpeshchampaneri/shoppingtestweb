09cd6c6a1b77b8ea3e372c7cc9ffe7277edfd3a0
alpesh <alpeshchampaneri541@gmail.com>
2026-09-19 01:26:40 +0530

    implemented firebase
    implemented firebase

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2153678654"
+    serviceWorkerVersion: "3300121635"
   }
 });
