289cdd827f1bf16f3aa8d11d05aa54bfd1f12f0e
alpesh <alpeshchampaneri541@gmail.com>
2026-09-20 00:44:25 +0530

    address improvement
    address improvement

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d9b85f2872681f40fd1520491d2d964a",
+const RESOURCES = {"flutter_bootstrap.js": "a2b7ce86e59ade898560e7177f96f9b1",
 "version.json": "95c0a1b4c07700629f9a4e40df80b362",
 "index.html": "9946579d750deb364f0537bf6334656c",
 "/": "9946579d750deb364f0537bf6334656c",
-"main.dart.js": "ac93ba47459895faa271531133f77df8",
+"main.dart.js": "eb74e9480f234198dc135594873e7657",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
